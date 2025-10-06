715b117ed9426d2a2422bf90fc69d792182e43da
ransubhegauri-svg <ransubhegauri@gmail.com>
2025-10-06 17:51:44 +0530

    done

--- 6_hello.c
+++ 6_hello.c
@@ -1,11 +1,15 @@
+//C Program to Multiply 2 floating point numbers
 #include<stdio.h>
 int main()
 {
     float num1,num2,product;
-    printf("\n enter any 2 decimal numbers:");
+    printf("\nenter any 2 decimal numbers:");
     scanf("%f %f",&num1,&num2);
     product=num1*num2;
-    printf("\n product=%.2f",product);
+    //output will be with 6 decimal places by default
+    //to change the number of decimal places,use %.nf where n is the number of decimal places
+    //for example,to print with 2 decimal places,use %.2f 
+    printf("\nproduct=%.2f  ",product);
     return 0;
 
 

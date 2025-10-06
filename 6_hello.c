ddf82a8ab6d7fc2ded166472a1be88afdb246021
ransubhegauri-svg <ransubhegauri@gmail.com>
2025-10-06 11:52:00 +0530

    done

---
+++ 6_hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float num1,num2,product;
+    printf("\n enter any 2 decimal numbers:");
+    scanf("%f %f",&num1,&num2);
+    product=num1*num2;
+    printf("\n product=%.2f",product);
+    return 0;
+
+
+}
